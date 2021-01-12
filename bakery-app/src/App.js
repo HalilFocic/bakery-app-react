5a30c3df9c92acf4ca037b99a4ee2eabbab812aa
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-12 21:34:09 +0100

    bakery setup

--- bakery-app/src/App.js
+++ bakery-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
+import Bakery from "./Bakery";
 import "./main.css";
 const App = () => {
   return (
     <>
       <Navbar />
+      <Bakery />
     </>
   );
 };
---
+++ bakery-app/src/Bakery.js
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from "react";
+const Bakery = () => {
+  return (
+    <div className="bakery-app">
+      Bakery App
+      <h3>imati listu hrane i kada ce klikne da izbaci racun</h3>
+    </div>
+  );
+};
+export default Bakery;
