bba36d77aee944a1710cc4fc31915288fe3aada0
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-12 21:16:51 +0100

    Navbar setup

---
+++ bakery-app/src/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <header className="nav__header">
+        <h2>Bakery App</h2>
+      </header>
+    </nav>
+  );
+};
+export default Navbar;
