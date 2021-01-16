e12e5ba36ce5509006d8d073542121e4e23fce26
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-16 15:34:24 +0100

    Fixed css

--- bakery-app/src/BakeryItem.jsx
+++ bakery-app/src/BakeryItem.jsx
@@ -8,20 +8,22 @@ const BakeryItem = ({ id, title, price, img, amount, text }) => {
       <img src={img} alt={title} className="bakery-grid__item__image" />
       <div className="bakery-grid__item__title">{title}</div>
       <div className="bakery-grid__item__desc">{text}</div>
-      <div className="bakery-grid__item__buttons">
-        <button
-          className="bakery-grid__item__button"
-          onClick={() => decreaseAmount(id)}
-        >
-          -
-        </button>
-        <span className="bakery-grid__item__amount">{amount}</span>
-        <button
-          className="bakery-grid__item__button"
-          onClick={() => increaseAmount(id)}
-        >
-          +
-        </button>
+      <div className="bakery-grid-down">
+        <div className="bakery-grid__item__buttons">
+          <button
+            className="bakery-grid__item__button"
+            onClick={() => decreaseAmount(id)}
+          >
+            -
+          </button>
+          <span className="bakery-grid__item__amount">{amount}</span>
+          <button
+            className="bakery-grid__item__button"
+            onClick={() => increaseAmount(id)}
+          >
+            +
+          </button>
+        </div>
         <div className="bakery-grid__item__price">${price * amount}</div>
       </div>
     </article>
